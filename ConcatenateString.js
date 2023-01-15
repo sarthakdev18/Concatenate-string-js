e4de1c0eebde64eceaa05bcb7b99312ce5957673
Sarthak Dev <91304126+sarthakdev18@users.noreply.github.com>
2023-01-15 16:46:43 +0530

    Add files via upload

---
+++ ConcatenateString.js
@@ -0,0 +1,10 @@
+function func() {
+  
+    // Original string
+    var str = 'It';
+  
+    // Joining the strings together
+    var value = str.concat(' is',' a',' great',' day!');
+    console.log(value);    
+}
+func();
